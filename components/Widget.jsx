440b9a6005af82e198028621b2ef4571b7652f0e
Ruslan Gainutdinov <ruslan@sanfrancisco.fi>
2023-06-12 13:37:56 +0300

    fix: animate tooltips

--- components/Widget.jsx
+++ components/Widget.jsx
@@ -3,8 +3,35 @@ import globe from "../public/globe.svg";
 import "../index.css";
 import { Tooltip } from "react-tooltip";
 
+const messages = [
+"You have a media hit!",
+"Journalist is interested in your story!",
+"Got 10 new followers!",
+"Your story is trending!",
+"10 likes on your post!",
+]
+
 export const Widget = ({ ...props }) => {
   const scale = props.scale || 1;
+  const [anchor, setAnchor] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  const showRandomMessage = useCallback(() => {
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    setMessage(randomMessage);
+    const randomAnchor = Math.floor(Math.random() * 7) + 1;
+    setAnchor("anchor" + randomAnchor);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      showRandomMessage();
+    }, 5000);
+
+    showRandomMessage();
+    return () => clearInterval(interval);
+  }, []);
+  
   return (
     <div
       className="hero"
@@ -14,19 +41,24 @@ export const Widget = ({ ...props }) => {
     >
       <img src={globe} width="693" />
       <div class="dot" id="anchor1" style={{ left: "221px", top: "247px" }}></div>
-      <div class="dot" style={{ left: "524px", top: "283px" }}></div>
-      <div class="dot" style={{ left: "522px", top: "370px" }}></div>
-      <Tooltip
-        anchorSelect="#anchor1"
-        content="You have a new media hit!"
+      <div class="dot" id="anchor2" style={{ left: "524px", top: "283px" }}></div>
+      <div class="dot" id="anchor3" style={{ left: "522px", top: "370px" }}></div>
+      <div class="dot" id="anchor4" style={{ left: "417px", top: "145px" }}></div>
+      <div class="dot" id="anchor5" style={{ left: "396px", top: "291px" }}></div>
+      <div class="dot" id="anchor6" style={{ left: "435px", top: "485px" }}></div>
+      <div class="dot" id="anchor7" style={{ left: "167px", top: "418px" }}></div>
+
+      {anchor  ? <Tooltip
+        anchorSelect={"#" + anchor}
+        content={message || "Test message"}
         variant="light"
         style={{
           boxShadow: "0px 1.8px 8.1px rgba(0, 0, 0, 0.25)",
           borderRadius: "4px",
-          padding: "8px 16px",
+          padding: "8px 16px"
         }}
-        isOpen={true}
-      />
+        isOpen
+      /> : null}
     </div>
   );
 };
